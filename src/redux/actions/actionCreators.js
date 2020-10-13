ccee445c498d1499c3b26dd8b5ecd93bd4c4b21e
mrh9ud <mrh9ud@virginia.edu>
2020-10-13 14:52:41 -0400

    added back methods that should have committed previously

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,5 +1,23 @@
-//import the strings for action types here from actionType.js
+import { VERIFY_USER_DATA } from './actionType'
 
-//implement the functions that generate actions for reducers here; will contain a type and a potential payload
+const userLoginUrl = "localhost:3000/api/v1/login"
 
-//export all of the functions
+function verifyUserData(userObj) {
+    return dispatch => {
+        const userConfigObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify(userObj)
+        }
+        dispatch(loading())    // need to  review docs to finish implementing this feature
+        fetch(userLoginUrl, userConfigObj)
+        .then(res => res.json())
+        .then(data => console.log(data))   // should call a method to set current user and store credentials on device
+        .catch(error => console.log(error.message))
+    }
+}
+
+export { verifyUserData }
