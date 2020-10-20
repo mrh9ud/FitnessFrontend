dd304f2ec7ce0e087bcbb7df31630c7a747f8044
rhode2nh <rhode2nh@protonmail.com>
2020-10-20 13:29:43 -0400

    added update fetch method. Setup basic patch form for editing user data

---
+++ src/helpers/FormKeyType.js
@@ -0,0 +1,7 @@
+const USERNAME = 'username'
+const PASSWORD = 'password'
+const FIRST_NAME = 'first_name'
+const LAST_NAME = 'last_name'
+const EMAIL = 'email'
+
+export { USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL }
