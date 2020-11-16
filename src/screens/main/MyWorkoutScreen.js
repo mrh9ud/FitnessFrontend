d413d8cc2faf690f5c7b937410f2b12a98f063d0
Nicholas Rhodes <rhode2nh@protonmail.com>
2020-11-16 13:09:16 -0500

    added create workout button

--- src/screens/main/MyWorkoutScreen.js
+++ src/screens/main/MyWorkoutScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Text} from "react-native-paper";
+import {Button, Text} from "react-native-paper";
 import { ScrollView } from 'react-native'
 import { connect } from 'react-redux'
 import WorkoutCard from "../../components/WorkoutCard";
@@ -12,6 +12,9 @@ const MyWorkoutScreen = ({ workouts, navigation }) => {
       :
         <Text>You currently don't have any workouts. Click the button below to generate a new workout.</Text>
       }
+      <Button onPress={() => navigation.navigate("Home", { screen: "Workout Questionnaire"})}>
+        Create New Workout
+      </Button>
     </ScrollView>
   )
 }
