79fef57d3d9ba15d8453c87032f60b17a16f7f2a
rhode2nh <rhode2nh@protonmail.com>
2020-11-18 15:35:27 -0500

    cleared navigation state when clicking drawer item

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -3,15 +3,32 @@ import { DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navi
 import { CommonActions } from "@react-navigation/native"
 
 const DrawerContent = ({ props, rootNavigation }) => {
+  const resetStackHistory = (name) => {
+    props.navigation.dispatch({
+      ...CommonActions.reset({
+        index: 0,
+        routes: [{ name }]
+      })
+    });
+  }
   return (
     <DrawerContentScrollView>
-      <DrawerItemList {...props} onItemPress={() => {
-      props.navigation.dispatch({
-      ...CommonActions.reset({
-        index: 0
-      })
-    })
-      }}/>
+      <DrawerItem label="Home" onPress={() => {
+        props.navigation.navigate("Home")
+        resetStackHistory("Home")
+      }} />
+      <DrawerItem label="My Workouts" onPress={() => {
+        props.navigation.navigate("My Workouts")
+        resetStackHistory("My Workouts")
+      }} />
+      <DrawerItem label="Stats" onPress={() => {
+        props.navigation.navigate("Stats")
+        resetStackHistory("Stats")
+      }} />
+      <DrawerItem label="Settings" onPress={() => {
+        props.navigation.navigate("Settings")
+        resetStackHistory("Settings")
+      }} />
       <DrawerItem label="Logout" onPress={() => rootNavigation.navigate("Auth", { screen: "Login" })} />
     </DrawerContentScrollView>
   )
